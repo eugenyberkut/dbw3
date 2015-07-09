88dc05c6244e0284aa65c586baf00f0c0035ecf8
Yevhen <eugeny.berkunsky@gmail.com>
2015-07-09 20:53:13 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,25 @@
  * Created by Yevhen on 09.07.2015.
  */
 import java.sql.*;
+import java.util.Properties;
 
 public class Main {
+
+    String login = "root";
+    String passw = "nbuser";
+
     public static void main(String[] args) {
         new Main().run();
     }
 
     private void run() {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lesson", "root", "nbuser");
+            Properties properties = new Properties();
+            properties.setProperty("user", login);
+            properties.setProperty("password", passw);
+            properties.setProperty("characterEncoding","utf8");
+            properties.setProperty("useUnicode", "true");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lesson", properties);
             Statement query = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
             ResultSet resultSet = query.executeQuery("SELECT * FROM person");
             while (resultSet.next()) {
@@ -20,6 +30,19 @@ public class Main {
                 int zp = resultSet.getInt("zp");
                 System.out.println(id + " " + firstname + " " + lastname + " " + zp);
             }
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            System.out.println(metaData.getColumnLabel(1));
+            System.out.println(metaData.getColumnLabel(2));
+            System.out.println(metaData.getColumnLabel(3));
+            System.out.println(metaData.getColumnLabel(4));
+
+            DatabaseMetaData metaData1 = connection.getMetaData();
+            System.out.println(metaData1.getSQLKeywords());
+//            PreparedStatement query2 = connection.prepareStatement("INSERT INTO person (firstname, lastname, zp) VALUES (? , ? , ?)");
+//            query2.setString(1, "Sergey");
+//            query2.setString(2, "Sidorov");
+//            query2.setInt(3, 2323);
+//            query2.executeUpdate();
         } catch (SQLException ex) {
             System.err.println("Error");
         }
